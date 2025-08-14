512dbd9cb56ff6576550f6d2dfe707c32c1f9da1
bigtr3 <tmddms1002@gmail.com>
2025-08-14 23:14:30 +0900

    feat : header 설정

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,7 @@
-<h1 class="text-green-400">Welcome to SvelteKit</h1>
-<p>
-  Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
-  documentation
-</p>
+<script>
+  import Hero from "../components/Hero.svelte";
+</script>
+
+<main class="flex flex-col">
+  <Hero />
+</main>
